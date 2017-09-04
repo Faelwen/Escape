2b444f6b29a40b214af61b406e227fca4d65b0b4
Faelwen <francis.john.mccarthy@googlemail.com>
2017-09-04 12:59:48 +0200

    Add runtime message

--- Source/Escape/PositionReporter.cpp
+++ Source/Escape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Escape.h"
 
 
 // Sets default values for this component's properties
@@ -10,7 +11,6 @@ UPositionReporter::UPositionReporter()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -19,7 +19,7 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
 	
 }
 
